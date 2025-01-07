68beef3a7f80e8fbe5527610c1aaf297ec41ffb5
jjs <jenjingsu860630@gmail.com>
2025-01-07 17:57:19 +0800

    還原原先漸層效果

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -18,45 +18,59 @@ const Contact = () => {
         const MIN_SPEED = 0.5;
         const MAX_SPEED = 2;
 
-        const randomNumber = (min, max) => Math.random() * (max - min) + min;
+        function randomNumber(min, max) {
+            return Math.random() * (max - min) + min;
+        }
 
         class Blob {
             constructor(el) {
                 this.el = el;
-                const { width } = el.getBoundingClientRect();
-                this.size = width;
-                this.x = randomNumber(0, window.innerWidth - this.size);
-                this.y = randomNumber(0, window.innerHeight - this.size);
+                const boundingRect = this.el.getBoundingClientRect();
+                this.size = boundingRect.width;
+                this.initialX = randomNumber(0, window.innerWidth - this.size);
+                this.initialY = randomNumber(0, window.innerHeight - this.size);
+                this.el.style.top = `${this.initialY}px`;
+                this.el.style.left = `${this.initialX}px`;
                 this.vx = randomNumber(MIN_SPEED, MAX_SPEED) * (Math.random() > 0.5 ? 1 : -1);
                 this.vy = randomNumber(MIN_SPEED, MAX_SPEED) * (Math.random() > 0.5 ? 1 : -1);
-                this.updatePosition();
-            }
-
-            updatePosition() {
-                this.el.style.top = `${this.y}px`;
-                this.el.style.left = `${this.x}px`;
+                this.x = this.initialX;
+                this.y = this.initialY;
             }
 
             update() {
                 this.x += this.vx;
                 this.y += this.vy;
+                if (this.x >= window.innerWidth - this.size) {
+                    this.x = window.innerWidth - this.size;
+                    this.vx *= -1;
+                }
+                if (this.y >= window.innerHeight - this.size) {
+                    this.y = window.innerHeight - this.size;
+                    this.vy *= -1;
+                }
+                if (this.x <= 0) {
+                    this.x = 0;
+                    this.vx *= -1;
+                }
+                if (this.y <= 0) {
+                    this.y = 0;
+                    this.vy *= -1;
+                }
 
-                if (this.x <= 0 || this.x >= window.innerWidth - this.size) this.vx *= -1;
-                if (this.y <= 0 || this.y >= window.innerHeight - this.size) this.vy *= -1;
-
-                this.el.style.transform = `translate(${this.x}px, ${this.y}px)`;
+                this.el.style.transform = `translate(${this.x - this.initialX}px, ${this.y - this.initialY}px)`;
             }
         }
 
-        const initBlobs = () => {
-            const blobs = Array.from(document.querySelectorAll(".blob")).map(el => new Blob(el));
+        function initBlobs() {
+            const blobEls = document.querySelectorAll(".blob");
+            const blobs = Array.from(blobEls).map((blobEl) => new Blob(blobEl));
 
-            const update = () => {
-                blobs.forEach(blob => blob.update());
+            function update() {
                 requestAnimationFrame(update);
-            };
+                blobs.forEach((blob) => blob.update());
+            }
             requestAnimationFrame(update);
-        };
+        }
 
         initBlobs();
     }, []);
